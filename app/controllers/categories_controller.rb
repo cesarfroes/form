59261d72322e911b07f62f5193a1c6e7ab8bde1c
cesarfroes <cesarfroes@gmail.com>
2015-04-02 00:02:46 -0300

    adding categories tests and pages

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,47 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
   
   def index
     @categories = Category.all()
   end
 
+  def new
+    @category = Category.new
+  end
 
+  def create
+    @category = Category.new(categories_params)
+
+    if @category.save
+      redirect_to categories_path, notice: 'Category was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  	
+  end  
+
+  def update
+    if @category.update(categories_params)
+      redirect_to categories_path, notice: 'Category was successfully updated.'
+    else
+      render :edit
+    end
+  end 
+
+  def destroy
+    @category.destroy
+    redirect_to categories_path, notice: 'Category was successfully destroyed.'
+  end 
+
+  private
+    def set_category
+      @category = Category.find(params[:id])
+    end
+
+    def categories_params
+      params.require(:category).permit(:name, :slug)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+ #get "/categories", to: "categories#index", as: "categories"
+ #get "/categories/new", to: "categories#new", as: "categories_new"
+ #post "/categories", to: "categories#create", as: "categories_create"
+ #get "/categories/:id/edit", to: "categories#edit", as: "categories_edit"
+ #delete "/categories/:id", to: "categories#destroy", as: "categories_destroy"
 
-get "categories", to: "categories#index"
-
+ resources :categories
 end
