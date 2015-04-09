4d230f1cf54ce31b519cd140c8bf5d7e4af6e201
cesarfroes <cesarfroes@gmail.com>
2015-04-08 23:24:26 -0300

    removing unnecessaries comments

--- app/models/field.rb
+++ app/models/field.rb
@@ -1,8 +1,7 @@
 class Field < ActiveRecord::Base
   belongs_to :sub_category
   belongs_to :field_element
-  #has_many :field_elements, inverse_of: :field
-
+  
   validates_format_of :title, :with => /^[^#\[\]{}$%&|!]+$/, :multiline => true
   validates :sub_category_id, :presence => true
   validates :field_element_id, :presence => true
--- app/models/field_element.rb
+++ app/models/field_element.rb
@@ -1,7 +1,6 @@
 class FieldElement < ActiveRecord::Base
     has_many :fields, inverse_of: :field_element
 
-	#belongs_to :field, inverse_of: :field_elements
 	validates :element, inclusion: %w(
 		                              check_box
 		                              password
